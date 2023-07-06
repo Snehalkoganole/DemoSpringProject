67af945207344614f399f79b4dca1cfe77103774
Snehalkoganole <snehalkog28@gmail.com>
2023-07-06 09:31:34 +0530

    Create PrintStatement.java

---
+++ src/main/java/com/SpringDemo/PrintStatement.java
@@ -0,0 +1,10 @@
+package com.SpringDemo;
+
+public class PrintStatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Welcome to github");
+	}
+
+}
