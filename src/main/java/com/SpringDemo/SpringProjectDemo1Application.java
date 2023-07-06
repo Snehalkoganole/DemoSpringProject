264ad0c96b363e5430410ba1914c1c97ddb45f25
Snehalkoganole <snehalkog28@gmail.com>
2023-07-06 09:12:10 +0530

    first commit

---
+++ src/main/java/com/SpringDemo/SpringProjectDemo1Application.java
@@ -0,0 +1,13 @@
+package com.SpringDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectDemo1Application.class, args);
+	}
+
+}
